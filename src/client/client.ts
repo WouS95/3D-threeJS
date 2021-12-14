52a21fd52f4c4767b072eb6c0c377bcc30d55c68
KoenLukassen <96050891+KoenLukassen@users.noreply.github.com>
2021-12-14 13:53:35 +0100

    homepage_index_v1
    Versiebeheer: v1

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,48 +1,98 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-const scene1 = new THREE.Scene()
-
-const camera1 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera1.position.z = 2
 
 
+// ----------------------------------------------------------------
+// HERO
+
+// Scene
+const hero = new THREE.Scene()
+hero.background = new THREE.Color(0)
+
+// Camera
+const heroCamera = new THREE.PerspectiveCamera(75, 1280 / 800, 0.1, 1000)
+heroCamera.position.z = 4
+
+// Lights
+hero.add(new THREE.AmbientLight(0xffffff, 0.25));
+const heroLight = new THREE.DirectionalLight(0xffff00, 1.0)
+
+// Renderer
 const canvas = document.getElementById("heroVisual") as HTMLCanvasElement
-const renderer = new THREE.WebGLRenderer({canvas:canvas})
-renderer.setSize(1200, 1200)
-document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer({canvas:canvas, antialias: true})
+renderer.setSize(1280, 800)
+// document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera1, renderer.domElement)
+new OrbitControls(heroCamera, renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
 
-const cube = new THREE.Mesh(geometry, material)
-scene1.add(cube)
+// 3D stuff
+const heroPlane = new THREE.PlaneBufferGeometry(50, 30, 300, 300);
 
-window.addEventListener('resize', onWindowResize, false)
-function onWindowResize() {
-    camera1.aspect = window.innerWidth / window.innerHeight
-    camera1.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
-}
+const plane = new THREE.Mesh(heroPlane, new THREE.MeshPhongMaterial({ color: 0xffffff }));
+plane.material.side = THREE.DoubleSide;
+plane.receiveShadow = true;
+plane.castShadow = true;
+plane.rotation.x = - Math.PI / 1.7;
+plane.position.y = -3
+// plane.rotation.y = - Math.PI / 1;
+
+
+// plane.position.z = - 30;
+hero.add(plane);
+hero.add(heroLight);
+
+
+// HERO
+//-----------------------------------------------------------------
+
+// window.addEventListener('resize', onWindowResize, false)
+// function onWindowResize() {
+//     camera1.aspect = window.innerWidth / window.innerHeight
+//     camera1.updateProjectionMatrix()
+//     renderer.setSize(window.innerWidth, window.innerHeight)
+//     render()
+// }
+
+const count: number = heroPlane.attributes.position.count;
+
 
 function animate() {
     requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    // plane.rotation.x += 0.05
+    // plane.rotation.y += 0.05
 
     // controls.update()
+        // SINE WAVE
+        // const now = Date.now() / 1000;
+        const now = Date.now() / 1000;
+        for (let i = 0; i < count; i++) {
+            const x = heroPlane.attributes.position.getX(i);
+            const y = heroPlane.attributes.position.getY(i);
+
+            // SINE WAVE
+            const xangle = x + now
+            const yangle = y + now
+            // const xangle = getRandomInt(2)
+
+            const xsin = Math.sin(xangle)
+            const ycos = Math.cos(yangle + 5)
+            heroPlane.attributes.position.setZ(i, xsin + ycos);
+        }
+        heroPlane.computeVertexNormals();
+        heroPlane.attributes.position.needsUpdate = true;
+
 
     render()
 }
 
+function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max)
+}
+
 function render() {
-    renderer.render(scene1, camera1)
+    renderer.render(hero, heroCamera)
 }
 animate()
