732e90bb2681c0200562d4c201c4e1c8f56cce20
neomie <noemiandriesse@gmail.com>
2021-12-17 10:00:57 +0100

    .

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,62 +6,75 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import gsap from 'gsap'
 import { GUI } from 'dat.gui'
 
-function log(message: string){
+function log(message: any){
     console.log(message)
 }
+let tl = gsap.timeline()
 
 const animationOne = document.getElementById('animationOne') as HTMLCanvasElement
+const animationTwo = document.getElementById('animationTwo') as HTMLCanvasElement
+const animationThree = document.getElementById('animationThree') as HTMLCanvasElement
 
 const sceneOne = new THREE.Scene()
 sceneOne.background = new THREE.Color(0x750319)
 
-let tl = gsap.timeline()
+const sceneTwo = new THREE.Scene()
+sceneTwo.background = new THREE.Color(0xffffff)
+
+const sceneThree = new THREE.Scene()
+sceneThree.background = new THREE.Color(0xffffff)
 
 const ambiLight = new THREE.AmbientLight(0xffffff, 1)
 const lightLeft = new THREE.PointLight(0xffffff, 1, 100)
-lightLeft.position.set( 35, 25, 50 );
+lightLeft.position.set( 0, 5, 20 );
 const lightRight = new THREE.PointLight(0xffffff, 1, 100)
-lightRight.position.set( 65, 75, 50 );
+lightRight.position.set( 0, 5, -20 );
+const ambiLight2 = new THREE.AmbientLight(0xffffff, 1)
+const lightLeft2 = new THREE.PointLight(0xffffff, 1, 100)
+lightLeft2.position.set( 0, 25, 50 );
+const lightRight2 = new THREE.PointLight(0xffffff, 1, 100)
+lightRight2.position.set( 0, 25, -50 );
+const ambiLight3 = new THREE.AmbientLight(0xffffff, 1)
+const lightLeft3 = new THREE.PointLight(0xffffff, 1, 100)
+lightLeft3.position.set( 0, 25, 50 );
+const lightRight3 = new THREE.PointLight(0xffffff, 1, 100)
+lightRight2.position.set( 0, 25, -50 );
 sceneOne.add(ambiLight)
 sceneOne.add(lightLeft)
 sceneOne.add(lightRight)
-
+sceneTwo.add(ambiLight2)
+sceneTwo.add(lightLeft2)
+sceneTwo.add(lightRight2)
+sceneThree.add(ambiLight3)
+sceneThree.add(lightLeft3)
+sceneThree.add(lightRight3)
 
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 4
 
+const orthocam = new THREE.OrthographicCamera(window.innerWidth / - 200, window.innerWidth / 200,window.innerHeight / 200, window.innerHeight / - 200, -10, 100)
 
-const renderer = new THREE.WebGLRenderer({canvas: animationOne, alpha: true})
-renderer.setPixelRatio(window.devicePixelRatio); 
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.setSize(window.innerWidth, window.innerHeight)
+const rendererOne = new THREE.WebGLRenderer({canvas: animationOne, alpha: true})
+rendererOne.setPixelRatio(window.devicePixelRatio); 
+rendererOne.outputEncoding = THREE.sRGBEncoding
+rendererOne.setSize(window.innerWidth, window.innerHeight)
+
+const rendererTwo = new THREE.WebGLRenderer({canvas: animationTwo, alpha: true})
+rendererTwo.setPixelRatio(window.devicePixelRatio); 
+rendererTwo.outputEncoding = THREE.sRGBEncoding
+rendererTwo.setSize(window.innerWidth, window.innerHeight)
+
+const rendererThree = new THREE.WebGLRenderer({canvas: animationThree, alpha: true})
+rendererThree.setPixelRatio(window.devicePixelRatio); 
+rendererThree.outputEncoding = THREE.sRGBEncoding
+rendererThree.setSize(window.innerWidth, window.innerHeight)
 
 // const controls = new OrbitControls(camera, renderer.domElement)
 // controls.enableDamping = true
 
     const loader = new GLTFLoader()
 
-    var phoneVar2 = new THREE.Object3D()
-    
-    loader.load('assets/noemi/phone/phone_v4.gltf', (gltf) => {
-        gltf.scene.position.set(-4,0.4,0)
-        gltf.scene.scale.set(0.4,0.4,0.4)
-        gltf.scene.traverse((child) => {
-            if (child instanceof THREE.Mesh){
-                console.log(child)
-                if (child.material.name == "phone_color"){
-                    child.material.color = new THREE.Color(0xFFD700)
-                }
-            }
-        })
-
-         phoneVar2 = gltf.scene
-        // sceneOne.add(gltf.scene)
-    })
-
     var phoneBlack = new THREE.Object3D ()
-    // function loadBlackPhone(){
-
     loader.load('assets/noemi/phone/phone_v3-2.gltf', (gltf) => {
         gltf.scene.position.set(-1.2,-3,0)
         gltf.scene.scale.set(0.2,0.2,0.2)
@@ -69,14 +82,9 @@ renderer.setSize(window.innerWidth, window.innerHeight)
             if (child instanceof THREE.Mesh){
                 if (child.material.name == "phone_color"){
                     child.material.color = new THREE.Color(0x000000)
-                    child.material.metalness = 0.8
+                    child.material.metalness = 0.9
                 }
             }
-        // tl.to(gltf.scene.position, { y:0.4, duration: 3})
-        // tl.to(gltf.scene.rotation, {y: 2.6, duration: 3}, "-=3")
-        // tl.to(gltf.scene.scale, {x: 0.25, y:0.25, z:0.25, duration:3}, "-=3")
-        // tl.to(gltf.scene.position, {x:1.5, duration:2})
-        // tl.to(gltf.scene.rotation, { y: 2.6, duration: 2}, "-=2")
         })
        
         phoneBlack = gltf.scene
@@ -102,17 +110,9 @@ renderer.setSize(window.innerWidth, window.innerHeight)
         LightRightFolder.add(lightRight.position, 'y', -10, 10)
         LightRightFolder.add(lightRight.position, 'z', -10, 10)
         phoneFolder.open()
-
-        // tl.to(gltf.scene.rotation, { y: 3.2, duration: 3})
-        // tl.to(gltf.scene.scale, {x: 0.25, y:0.25, z:0.25, duration:3}, "-=3")
-        // tl.to(gltf.scene.position, {x:1.5, duration:2})
-        // tl.to(gltf.scene.rotation, { y: 2.6, duration: 2}, "-=2")
     })
-// }
 
     var phoneWhite = new THREE.Object3D ()
-
-    // function loadWhitePhone(){
     loader.load('assets/noemi/phone/phone_v3-2.gltf', (gltf) => {
         gltf.scene.position.set(1.2,-3,0)
         gltf.scene.scale.set(0.2,0.2,0.2)
@@ -125,36 +125,13 @@ renderer.setSize(window.innerWidth, window.innerHeight)
                     child.material.roughness = 0.7
                 }
             }
-        // tl.to(gltf.scene.position, { y:0.4, duration: 3})
-        // tl.to(gltf.scene.rotation, {y: 2.6, duration: 3}, "-=3")
         })
         
         phoneWhite = gltf.scene
         sceneOne.add(gltf.scene)
     })
-// }
-
-    var phoneSilver = new THREE.Object3D ()
-    // function loadSilverPhone(){
-    loader.load('assets/noemi/phone/phone_v3-2.gltf', (gltf) => {
-        // gltf.scene.position.set(0,0.4,0)
-        gltf.scene.scale.set(0.4,0.4,0.4)
-        gltf.scene.traverse((child) => {
-            if (child instanceof THREE.Mesh){
-                if (child.material.name == "phone_color"){
-                    child.material.color = new THREE.Color(0xcecece)
-                    child.material.metalness = 0.8
-                }
-            }
-        })
-        
-        phoneSilver = gltf.scene
-        // sceneOne.add(gltf.scene)
-    })
-// }
 
     var phoneGold = new THREE.Object3D ()
-    // function loadGoldPhone(){
     loader.load('assets/noemi/phone/phone_v3-2.gltf', (gltf) => {
         gltf.scene.position.set(0,-3,0)
         gltf.scene.scale.set(0.2,0.2,0.2)
@@ -166,91 +143,155 @@ renderer.setSize(window.innerWidth, window.innerHeight)
                     child.material.metalness = 0.8
                 }
             }
-        // tl.to(gltf.scene.position, { y:0.4, duration: 3})
-        // tl.to(gltf.scene.rotation, {y: 2.6, duration: 3}, "-=3")
         })
         
         phoneGold = gltf.scene
         sceneOne.add(gltf.scene)
     })
-// }
+
+    var phoneVar2 = new THREE.Object3D()
+    loader.load('assets/noemi/phone/phone_v4.gltf', (gltf) => {
+        gltf.scene.position.set(0,0.15,-0.1)
+        gltf.scene.scale.set(0.2,0.2,0.2)
+        gltf.scene.traverse((child) => {
+            if (child instanceof THREE.Mesh){
+                console.log(child)
+                if (child.material.name == "phone_color"){
+                    child.material.color = new THREE.Color(0xcecece)
+                    child.material.metalness = 0.95
+                }
+            }
+        })
+        sceneTwo.add(gltf.scene)
+        phoneVar2 = gltf.scene
+    })
+    var phoneSilver = new THREE.Object3D ()
+    loader.load('assets/noemi/phone/phone_v3-2.gltf', (gltf) => {
+        gltf.scene.position.set(0,0.15,0.1)
+        gltf.scene.rotation.y = 3.1
+        gltf.scene.scale.set(0.18,0.18,0.18)
+        gltf.scene.traverse((child) => {
+            if (child instanceof THREE.Mesh){
+                // phoneSilver = child as THREE.Mesh
+                if (child.material.name == "phone_color"){
+                    child.material.color = new THREE.Color(0xcecece)
+                    child.material.metalness = 0.95
+                }
+            }
+        })
+        sceneTwo.add(gltf.scene)
+        phoneSilver = gltf.scene
+    })
+    // sceneTwo.overrideMaterial = new THREE.MeshBasicMaterial({color: 'gold'});
+ 
+    var phoneBlueOne = new THREE.Object3D ()
+    var phoneBlueTwo = new THREE.Object3D ()
+    var phoneBlueThree = new THREE.Object3D ()
+    var phoneBlueFour = new THREE.Object3D ()
+
+    loader.load('assets/noemi/phone/phone_v3-2.gltf', (gltf) => {
+        // gltf.scene.position.set(1.2,-3,0)
+        gltf.scene.scale.set(0.16,0.16,0.16)
+
+        gltf.scene.traverse((child) => {
+            if (child instanceof THREE.Mesh){
+                if (child.material.name == "phone_color"){
+                    child.material.color = new THREE.Color(0x8fc9d9)
+                    child.material.metalness = 0.6
+                    child.material.roughness = 0.7
+                }
+            }
+        })
+        
+        var bluePhone = gltf.scene
+        var phoneRotation = 0
+        for (var i =0 ; i<5; i++){
+            var newModel = bluePhone.clone()
+            newModel.position.set(-3.5+1.75*i, -3.5+1.75*i , 0)
+            newModel.rotation.z = i*(.5*Math.PI)
+            sceneThree.add(newModel)
+            tl.to(newModel.rotation, {z: i*(.5*Math.PI)+Math.PI , duration: 1} )
+        }
+        // gltf.scene.position.set(-2.5,-2.5,0)
+        // phoneBlueOne = gltf.scene
+        // sceneThree.add(phoneBlueOne)
+        // gltf.scene.position.set(2.5,2.5,0)
+        // phoneBlueTwo = gltf.scene
+        // sceneThree.add(phoneBlueTwo)
+    })
 
     window.addEventListener('resize', onWindowResize, false)
     function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
+    rendererOne.setSize(window.innerWidth, window.innerHeight)
+    rendererTwo.setSize(window.innerWidth, window.innerHeight)
+    rendererThree.setSize(window.innerWidth, window.innerHeight)
     render()
 }
+let playAnimationTwo = false
+window.addEventListener("scroll", function() {
+const t = Math.abs(document.body.getBoundingClientRect().top)
+log(t)
+log(window.innerHeight)
 
-// function animationSceneOne(){
-//     tl.to(phoneBlack.position, { y:0.4, duration: 3})
-//     tl.to(phoneBlack.rotation, {y: 2.6, duration: 3}, "-=3")
-//     tl.to(phoneGold.position, { y:0.4, duration: 3})
-//     tl.to(phoneGold.rotation, {y: 2.6, duration: 3}, "-=3")
-//     tl.to(phoneWhite.position, { y:0.4, duration: 3})
-//     tl.to(phoneWhite.rotation, {y: 2.6, duration: 3}, "-=3")
-//     // phoneBlack.position.set(-4,5,1)
-// }
+if(t > window.innerHeight){
+    log("start animation 2")
+    playAnimationTwo = true
+}
+else {
+    playAnimationTwo = false
+}
+})
 
-// animationSceneOne()
-// var scaleup =false
-// var scaleforzoom = 1
-// window.addEventListener('mousemove', e =>{
+const textTexture= new TextureLoader().load('assets/noemi/phone/knipsel.PNG')
+const textGeometry = new THREE.PlaneBufferGeometry(3,2)
+const textMaterial = new THREE.MeshBasicMaterial({map: textTexture})
+const textFieldPhones = new THREE.Mesh(textGeometry, textMaterial)
+textFieldPhones.position.z = -0.5
+textFieldPhones.position.y = 5
 
-//     var mouse = new THREE.Vector2();
-//     mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-//     mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
-
-//     var raycaster = new THREE.Raycaster();
-//     raycaster.setFromCamera( mouse, camera );
-//     var intersects = raycaster.intersectObject( imageMesh );
-
-//     if(intersects.length > 0) {
-//         if (scaleup == false && scaleforzoom==1){
-//             scaleup=true
-//         }
-//         else if (scaleup==true &&scaleforzoom>=1.2){
-//             scaleup=false
-//         }
-//     }
-
-// })
-
-
-
+sceneTwo.add(textFieldPhones)
 
 function animate() {
     requestAnimationFrame(animate)
 
-    // phone.rotation.x += 0.01
-    // phone.rotation.y += 0.01
-    // if (scaleup && scaleforzoom <1.2){
-    //     scaleforzoom+=0.01
-    //         imageMesh.scale.set(scaleforzoom,scaleforzoom, 1)
-    // }
-    // controls.update()
-
-    // phoneBlack.rotation.y +=0.05
-
     if(phoneBlack.position.y <0.25){
-
         phoneBlack.position.y +=0.0149
         phoneBlack.rotation.y+=0.013
         phoneBlack.scale.x +=0.0001
-        // phoneBlack.position.x +=0.0002
         phoneGold.position.y +=0.015
         phoneGold.rotation.y +=0.013
         phoneWhite.position.y +=0.015
         phoneWhite.rotation.y +=0.013
         phoneWhite.scale.x -=0.0001
-        // phoneWhite.position.x +=0.0003
     }
+    if (playAnimationTwo){
+        if (phoneVar2.position.x > -2.5){
+            phoneVar2.position.x -= 0.025
+            phoneVar2.rotation.y += 0.008
+            phoneVar2.scale.x -= 0.0002
+        }
+        if (phoneSilver.position.x < 2.5){
+            phoneSilver.position.x += 0.025
+            // phoneSilver.position.z += 0.0001
+            phoneSilver.rotation.y -= 0.008
+            phoneSilver.scale.x -= 0.0002
+        }
+        if (textFieldPhones.position.y > 0.25){
+            textFieldPhones.position.y -= 0.045
+            log("dropping tekst")
+        }
+
+    }
+
     render()
 }
 
 function render() {
-    renderer.render(sceneOne, camera)
+    rendererOne.render(sceneOne, camera)
+    rendererTwo.render(sceneTwo, camera)
+    rendererThree.render(sceneThree, orthocam)
 }
 
 animate()
