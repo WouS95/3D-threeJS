721cb0618d184aaade423bf01d10e273215278ff
bram-huinink <bram@huinink.net>
2021-12-19 14:28:54 +0100

    eerste opzet productpage

--- src/client/client.ts
+++ src/client/client.ts
@@ -25,7 +25,7 @@ scene1.add(ambientLight)
 
 const spotLight = new THREE.SpotLight()
 spotLight.position.set(0,0,3)
-spotLight.intensity = 0.6
+spotLight.intensity = 0.8
 scene1.add(spotLight)
 
 // const controls = new OrbitControls(camera, renderer.domElement)
@@ -50,21 +50,6 @@ loader.load(
         console.log(error)
     }
 )
-
-// const gui = new GUI()
-// const lightFolder = gui.addFolder('THREE.Light')
-// lightFolder.add(spotLight, 'intensity', 0, 1, 0.01)
-// lightFolder.open()
-// const spotLightFolder = gui.addFolder('THREE.SpotLight')
-// spotLightFolder.add(spotLight, 'distance', 0, 100, 0.01)
-// spotLightFolder.add(spotLight, 'decay', 0, 4, 0.1)
-// spotLightFolder.add(spotLight, 'angle', 0, 1, 0.1)
-// spotLightFolder.add(spotLight, 'penumbra', 0, 1, 0.1)
-// spotLightFolder.add(spotLight.position, 'x', -50, 50, 0.01)
-// spotLightFolder.add(spotLight.position, 'y', -50, 50, 0.01)
-// spotLightFolder.add(spotLight.position, 'z', -50, 50, 0.01)
-// spotLightFolder.open()
-
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -96,6 +81,173 @@ function introAnimation() {
     })
 }
 
+const scene2 = new THREE.Scene()
+
+let headphone2: any
+
+const camera2 = new THREE.PerspectiveCamera(75, window.innerWidth/2 / window.innerHeight, 0.1, 1000)
+camera2.position.z = 1
+camera2.position.y = 0.5
+camera2.position.x = -0
+
+const canvas2 = document.getElementById("canvas2") as HTMLCanvasElement
+const renderer2 = new THREE.WebGLRenderer({
+    canvas: canvas2,
+    alpha: true
+})
+renderer2.setSize(window.innerWidth/2, window.innerHeight)
+
+
+const spotLight2 = new THREE.SpotLight()
+spotLight2.position.set(0,0,3)
+spotLight2.intensity = 2
+scene2.add(spotLight2)
+
+
+loader.load(
+    'assets/bram/headphone/uploads_files_3333731_Headphone.gltf',
+    function (gltf) {
+        gltf.scene.scale.x = 0.4
+        gltf.scene.scale.y = 0.4
+        gltf.scene.scale.z = 0.4
+        gltf.scene.position.y = 0.7
+        scene2.add(gltf.scene)
+        headphone2 = gltf
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+
+
+const scene3 = new THREE.Scene()
+
+let headphone3: any
+
+const camera3 = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera3.position.z = 6
+camera3.position.y = -4.8
+camera3.position.x = 0.1
+
+
+const canvas3 = document.getElementById("canvas3") as HTMLCanvasElement
+const renderer3 = new THREE.WebGLRenderer({
+    canvas: canvas3,
+    alpha: true
+})
+renderer3.setSize(window.innerWidth, window.innerHeight)
+
+const ambientLight3 = new THREE.AmbientLight()
+ambientLight3.intensity = 0.8
+scene3.add(ambientLight3)
+
+const spotLight3 = new THREE.SpotLight()
+spotLight3.position.set(0,3,3)
+spotLight3.intensity = 2
+scene3.add(spotLight3)
+
+loader.load(
+    'assets/bram/headphone/uploads_files_3333731_Headphone.gltf',
+    function (gltf) {
+        scene3.add(gltf.scene)
+        headphone3 = gltf
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+const scene4 = new THREE.Scene()
+
+let headphone4: any
+
+const camera4 = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera4.position.z = 7
+camera4.position.y = 0.5
+
+
+const canvas4 = document.getElementById("canvas4") as HTMLCanvasElement
+const renderer4 = new THREE.WebGLRenderer({
+    canvas: canvas4,
+    alpha: true
+})
+renderer4.setSize(window.innerWidth, window.innerHeight)
+
+const ambientLight4 = new THREE.AmbientLight()
+ambientLight4.intensity = 0.2
+scene4.add(ambientLight4)
+
+const spotLight4 = new THREE.SpotLight()
+spotLight4.position.set(0,3,3)
+spotLight4.intensity = 2
+scene4.add(spotLight4)
+
+loader.load(
+    'assets/bram/headphone/uploads_files_3333731_Headphone.gltf',
+    function (gltf) {
+        gltf.scene.rotation.y = Math.PI / 2 
+        gltf.scene.position.x = 0.24
+        scene4.add(gltf.scene)
+        headphone4 = gltf
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+
+const scene5 = new THREE.Scene()
+
+let headphone5: any
+
+const camera5 = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera5.position.z = 15
+camera5.position.y = -1.5
+camera5.position.x = -0.7
+
+const ambientLight5 = new THREE.AmbientLight()
+ambientLight5.intensity = 0.5
+scene5.add(ambientLight5)
+
+const spotLight5 = new THREE.SpotLight()
+spotLight5.position.set(0,0,5)
+spotLight5.intensity = 1
+scene5.add(spotLight5)
+
+
+
+const canvas5 = document.getElementById("canvas5") as HTMLCanvasElement
+const renderer5 = new THREE.WebGLRenderer({
+    canvas: canvas5,
+    alpha: true
+})
+renderer5.setSize(window.innerWidth/2, window.innerHeight/2)
+
+loader.load(
+    'assets/bram/headphone/uploads_files_3333731_Headphone.gltf',
+    function (gltf) {
+        scene5.add(gltf.scene)
+        headphone5 = gltf
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
+
+
 window.onscroll = function(){
     console.log(window.scrollY)
     if(window.scrollY > 1000){
@@ -108,8 +260,52 @@ window.onscroll = function(){
     } else {
         hero.style.opacity = "1"
     }
+    if(window.scrollY > 800){
+        const soundSpecsText = document.getElementById("soundSpecsTextDiv")
+        gsap.to(soundSpecsText, {
+            duration: 2,
+            css: {
+                opacity: 1,
+                top: 250,
+            },
+        })
+    }
+    if(window.scrollY > 1500){
+        gsap.to(camera3.position, {
+            duration: 1,
+            y: -3.8,
+        })
+    }
+    if(window.scrollY > 1700){
+        const micSpecsText = document.getElementById("micSpecsText")
+        gsap.to(micSpecsText, {
+            duration: 2,
+            css: {
+                marginTop: -450,
+                opacity: 1,
+            },
+        })
+    }
+    if(window.scrollY > 2500){
+        gsap.to(headphone4.scene.rotation, {
+            duration: 1,
+            y: 0,
+        })
+        gsap.to(headphone4.scene.position, {
+            duration: 1,
+            x: -0.1,
+        })
+        const bandSpecsText = document.getElementById("bandSpecsText")
+        gsap.to(bandSpecsText, {
+            delay: 0.5,
+            duration: 2,
+            css: {
+                top: 400,
+                opacity: 1,
+            },
+        })
+    }
 }
-
 
 function animate() {
     requestAnimationFrame(animate)
@@ -118,13 +314,21 @@ function animate() {
         headphone1.scene.scale.y = 0.4 + window.scrollY * 0.0005
         headphone1.scene.scale.x = 0.4 + window.scrollY * 0.0005
         headphone1.scene.position.y = 0.8 + window.scrollY * 0.001
+
+        headphone2.scene.rotation.y = window.scrollY * 0.0005
+        camera2.position.y = 0.5 - window.scrollY * 0.0005
+
+        headphone5.scene.rotation.y += 0.005
     }
     // controls.update()
 
     render()
 }
-
 function render() {
     renderer.render(scene1, camera)
+    renderer2.render(scene2, camera2)
+    renderer3.render(scene3, camera3)
+    renderer4.render(scene4, camera4)
+    renderer5.render(scene5, camera5)
 }
 animate()
