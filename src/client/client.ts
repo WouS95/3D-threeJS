393be08eaef52591b4ba2de5ad4f3d79347a4c62
KoenLukassen <96050891+KoenLukassen@users.noreply.github.com>
2022-01-17 10:14:50 +0100

    17/01

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,6 +4,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GUI } from "dat.gui"
 import { Raycaster } from 'three'
 import gsap from "gsap";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
 
 
 
@@ -16,20 +18,44 @@ import gsap from "gsap";
 // Scene
 const hero = new THREE.Scene()
 const visual = new THREE.Scene()
+const product = new THREE.Scene()
 hero.background = new THREE.Color(0)
 // visual.background = new THREE.Color(0xe8e8e8)
 
 // Camera
-const heroCamera = new THREE.PerspectiveCamera(75, 1280 / 800, 0.1, 1000)
+const heroCamera = new THREE.PerspectiveCamera(75, 1920 / 800, 0.1, 1000)
 const visualCamera = new THREE.PerspectiveCamera(75, 1280 / 800, 0.1, 1000)
+const productCamera = new THREE.PerspectiveCamera(75, 1920 / 800, 0.1, 1000)
 heroCamera.position.z = 4
 visualCamera.position.z = 2
+productCamera.position.z = 3
 
+let headphone1: any
+const loader = new GLTFLoader()
+loader.load(
+    'assets/bram/headphone2/scene.gltf',
+    function (gltf) {
+        gltf.scene.scale.x = 0.01
+        gltf.scene.scale.y = 0.01
+        gltf.scene.scale.z = 0.01
+        gltf.scene.position.z = 1
+        gltf.scene.rotation.y = Math.PI/1.13
+        product.add(gltf.scene)
+        headphone1 = gltf
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    },
+    (error) => {
+        console.log(error)
+    }
+)
 
 // TextureLoader
 const textureLoader = new THREE.TextureLoader()
 
 const geometry = new THREE.PlaneBufferGeometry(1, 1.3)
+// const geometry2 = new THREE.PlaneBufferGeometry(1, 1.3)
 
 for (let i = 0; i < 3; i++) {
     const material = new THREE.MeshBasicMaterial({
@@ -60,7 +86,6 @@ visual.traverse((object) => {
 
 const sphereSize = 10;
 
-
 const light1 = new THREE.PointLight(0x973999, 2)
 hero.add(light1)
 const pointLightHelper1 = new THREE.PointLightHelper(light1, sphereSize);
@@ -81,10 +106,29 @@ hero.add(light4)
 const pointLightHelper4 = new THREE.PointLightHelper(light4, sphereSize);
 // hero.add( pointLightHelper4 );
 
+
+const light5 = new THREE.PointLight(0xffffff, 4)
+product.add(light5)
+const pointLightHelper5 = new THREE.PointLightHelper(light5, sphereSize);
+// product.add(pointLightHelper5)
+
+// PointLight( color : Integer, intensity : Float, distance : Number, decay : Float )
+const light6 = new THREE.PointLight(0x000fff, 2, 0, 40)
+product.add(light6)
+const pointLightHelper6 = new THREE.PointLightHelper(light6, sphereSize);
+// product.add(pointLightHelper6)
+
+const light7 = new THREE.PointLight(0x0f00ff, 2, 0, 40)
+product.add(light7)
+const pointLightHelper7 = new THREE.PointLightHelper(light7, sphereSize);
+// product.add(pointLightHelper7)
+
 light1.position.set(5, 5, 5)
 light2.position.set(15, -5, 15)
 light3.position.set(0, -5, 5)
 light3.position.set(0, -3, 6)
+light5.position.set(0, -3, 6)
+light6.position.set(2, -3, 6)
 
 // Lights VISUAL
 
@@ -95,17 +139,24 @@ light3.position.set(0, -3, 6)
 // Renderer
 const canvas = document.getElementById("heroVisual") as HTMLCanvasElement
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true })
-renderer.setSize(1280, 800)
+renderer.setSize(1920, 800)
 
 const canvas2 = document.getElementById("carrouselVisual") as HTMLCanvasElement
 const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2, antialias: true })
 renderer2.setSize(1280, 800)
 
+const canvas3 = document.getElementById("overzichtVisual") as HTMLCanvasElement
+const renderer3 = new THREE.WebGLRenderer({ canvas: canvas3, antialias: true })
+renderer3.setSize(1920, 800)
+
+
 // new OrbitControls(heroCamera, renderer.domElement)
 // new OrbitControls(visualCamera, renderer2.domElement)
+// new OrbitControls(productCamera, renderer3.domElement)
 
 // 3D stuff
 const heroPlane = new THREE.PlaneBufferGeometry(30, 30, 250, 350);
+const headphonePlane = new THREE.PlaneBufferGeometry(30, 30, 250, 350);
 
 // const plane = new THREE.Mesh(heroPlane, new THREE.MeshNormalMaterial({}));
 const plane = new THREE.Mesh(heroPlane, new THREE.MeshPhysicalMaterial({
@@ -118,8 +169,21 @@ const plane = new THREE.Mesh(heroPlane, new THREE.MeshPhysicalMaterial({
     // wireframe: true
 }))
 
-const visualPlane = new THREE.BoxGeometry(1, 1, 1, 1)
-const gallery = new THREE.Mesh(visualPlane, new THREE.MeshNormalMaterial({}))
+// const plane2 = new THREE.Mesh(headphonePlane, new THREE.MeshNormalMaterial({}));
+const plane2 = new THREE.Mesh(headphonePlane, new THREE.MeshPhysicalMaterial({
+    // color: 0x00ff00,
+    roughness: 0,
+    metalness: 1,
+    reflectivity: 1,
+    clearcoat: 1,
+    clearcoatRoughness: 0,
+    // wireframe: true
+}))
+
+
+
+// const visualPlane = new THREE.BoxGeometry(1, 1, 1, 1)
+// const gallery = new THREE.Mesh(visualPlane, new THREE.MeshNormalMaterial({}))
 
 plane.material.side = THREE.DoubleSide;
 plane.receiveShadow = true;
@@ -128,13 +192,25 @@ plane.rotation.x = - Math.PI / 1;
 plane.position.y = -3
 plane.rotation.y = - Math.PI / 1;
 
+plane2.material.side = THREE.DoubleSide;
+plane2.receiveShadow = true;
+plane2.castShadow = true;
+plane2.rotation.x = - Math.PI / 1;
+plane2.position.y = -1
+plane2.position.z = -3
+plane2.rotation.y = - Math.PI / 1;
+// plane2.rotation.z += 0.2
+
+
 hero.add(plane)
+product.add(plane2)
 // visual.add(gallery)
 
 // HERO
 //-----------------------------------------------------------------
 
 const count: number = heroPlane.attributes.position.count;
+const count2: number = headphonePlane.attributes.position.count;
 
 
 // Mouse
@@ -161,6 +237,7 @@ function animate() {
     animateLights()
 
     plane.rotation.z += 0.0015
+    // headphone1.rotation.y += 0.05
 
     // SINE WAVE
     // const now = Date.now() / 1000;
@@ -181,13 +258,28 @@ function animate() {
     heroPlane.computeVertexNormals();
     heroPlane.attributes.position.needsUpdate = true;
 
-    gallery.rotation.x += .05
+    for (let i = 0; i < count; i++) {
+        const x = heroPlane.attributes.position.getX(i);
+        const y = heroPlane.attributes.position.getY(i);
+
+        // SINE WAVE
+        const xangle = x + now
+        const yangle = y + now
+        // const xangle = getRandomInt(2)
+
+        const xsin = Math.sin(xangle * 4)
+        const ycos = Math.cos(yangle * 5)
+        headphonePlane.attributes.position.setZ(i, xsin);
+    }
+    headphonePlane.computeVertexNormals();
+    headphonePlane.attributes.position.needsUpdate = true;
+
+    // gallery.rotation.x += .05
 
     if ( window.scrollY > 0 ) {
         position += y
         y *= .9
     }
-    
 
     showSpecs()
 
@@ -282,6 +374,7 @@ function showSpecs(){
 function render() {
     renderer.render(hero, heroCamera)
     renderer2.render(visual, visualCamera)
+    renderer3.render(product, productCamera)
 }
 
 function animateLights() {
@@ -300,6 +393,12 @@ function animateLights() {
 
     light4.position.x = Math.sin(time * 0.2) * d;
     light4.position.z = Math.cos(time * 0.5) * d;
+
+    light6.position.y = Math.sin(time * 0.3) * d;
+    light6.position.z = Math.cos(time * 0.1) * d;
+
+    light7.position.y = Math.sin(time * 0.1) * d;
+    light7.position.z = Math.cos(time * 0.3) * d;
 }
 
 animate()
